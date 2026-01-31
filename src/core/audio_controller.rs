b2323bf5c58172f63407606130eff894639148bd
Jerthz <43185161+jerthz@users.noreply.github.com>
2026-02-01 00:12:57 +0100

    fix: audio thread loop was dead

--- src/core/audio_controller.rs
+++ src/core/audio_controller.rs
@@ -21,35 +21,42 @@ impl AudioController {
 pub(crate) fn audio_thread(controller: AudioController) {
     let stream_handle = OutputStreamBuilder::open_default_stream()
         .expect("open default audio stream");
-    let mut sinks: HashMap<usize, Sink> = HashMap::new();
+    let mut sinks: HashMap<usize, (Sink, bool)> = HashMap::new();
 
     loop {
-        if let Ok(message) = controller.receiver.recv_timeout(Duration::from_secs(1)) {
+        if let Ok(message) = controller.receiver.recv_timeout(Duration::from_millis(100)) {
             match message {
                 AudioEvent::PlaySound { path, config, sound_id } => {
                     debug!("Started to play sound {}", path);
                     let sink =  rodio::Sink::connect_new(&stream_handle.mixer());
-                    let file = std::fs::File::open(path.as_str()).unwrap();
-                    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
                     if config.looped {
-                        sink.append(source.repeat_infinite());
+                        let file = std::fs::File::open(path.as_str()).unwrap();
+                        let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+                        let buf_source = rodio::buffer::SamplesBuffer::new(
+                            source.channels(),
+                            source.sample_rate(),
+                            source.collect::<Vec<_>>()
+                        );
+                        sink.append(buf_source.repeat_infinite());
                     } else {
+                        let file = std::fs::File::open(path.as_str()).unwrap();
+                        let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
                         sink.append(source);
                     }
                     // TODO: handle categories
                     sink.set_volume(config.volume);
-                    sinks.insert(sound_id, sink);
+                    sink.play();
+                    sinks.insert(sound_id, (sink, config.looped));
                 }
                 AudioEvent::StopSound { sound_id } => {
-                    if let Some(sink) = sinks.remove(&sound_id) {
+                    if let Some((sink, _)) = sinks.remove(&sound_id) {
                         sink.stop();
                         drop(sink);
                     }
                 }
             }
+            // Only clean up finished sinks when we receive a message
+            sinks.retain(|&_k, (sink, looped)| if *looped { true } else { !sink.empty() });
         }
-        sinks.retain(|&_k, sink| {
-            !sink.empty()
-        });
     }
 }
